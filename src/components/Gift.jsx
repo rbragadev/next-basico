d4d89654c1d84b80f5cdebf5f0ca000e6c5474df
Raphael Braga <raffael.info@gmail.com>
2023-12-18 14:14:00 -0300

    :rocket: feat: css do presente

---
+++ src/components/Gift.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Gift.module.css';
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.ribbon}></div>
+      <div className={styles.ribbon2}></div>
+    </div>
+  );
+}
