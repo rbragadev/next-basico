e462234ce4c0cbce065dd132bb5a821503cdb045
Raphael Braga <raffael.info@gmail.com>
2023-12-18 17:46:38 -0300

    :rocket: feat: utilizando o calc para maçaneta

--- src/components/Door.jsx
+++ src/components/Door.jsx
@@ -2,8 +2,14 @@ import styles from '../styles/Door.module.css';
 
 export default function Door(props) {
   return (
-    <div className={styles.door}>
-      <div className={styles.number}>3</div>
+    <div className={styles.area}>
+      <div className={styles.frame}>
+        <div className={styles.door}>
+          <div className={styles.number}>3</div>
+          <div className={styles.doorknob}></div>
+        </div>
+      </div>
+      <div className={styles.floor}></div>
     </div>
   );
 }
