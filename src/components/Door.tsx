1e9c199717fb295e13b64d6103f2538364217476
Raphael Braga <raffael.info@gmail.com>
2023-12-26 17:52:16 -0300

    :rocket: feat: eventos de seleçao da porta

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -2,21 +2,35 @@ import DoorModel from '@/model/door';
 import styles from '../styles/Door.module.css';
 
 interface DoorProps {
-  door: DoorModel;
+  value: DoorModel;
+  onChange: (newDoor: DoorModel) => void;
 }
 
 export default function Door(props: DoorProps) {
-  const { door } = props;
-  const doorSelected = door.selected ? styles.frameSelected : '';
+  const door = props.value;
+  const doorSelected = door.selected && !door.open ? styles.frameSelected : '';
+
+  const alterSelect = (e: any) => props.onChange(door.alterSelect());
+  const open = (e: any) => {
+    e.stopPropagation();
+    props.onChange(door.openDoor());
+  };
+
+  function renderDoor() {
+    return (
+      <div className={styles.door}>
+        <div className={styles.number}>{door.number}</div>
+        <div className={styles.doorknob} onClick={open}></div>
+      </div>
+    );
+  }
 
   return (
-    <div className={styles.area}>
+    <div className={styles.area} onClick={alterSelect}>
       <div className={`${styles.frame} ${doorSelected}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{door.number}</div>
-          <div className={styles.doorknob}></div>
-        </div>
+        {door.open ? false : renderDoor()}
       </div>
+
       <div className={styles.floor}></div>
     </div>
   );
